a145fc03bb4fdc94e420091a19f3fa929e9c6f3e
Chris <nunezchristian92@gmail.com>
2022-08-07 19:36:51 -0700

    added json

---
+++ Develop/server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers} = require('../schemas');
+const {authMiddleware} = require('./utils/auth')
+
+// const db = require('./config/connection')
+ 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// apply apollo server with express app
+server.applyMiddleware({app})
+
+// parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+//  get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
